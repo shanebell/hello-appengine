47d2ef5c19e4f1a8fca09c66878d99d5ba76b1b4
Shane Bell <shane.bell@gmail.com>
2019-05-27 10:05:38 +1000

    Tidy up

--- src/main/java/hello/controller/HelloController.java
+++ src/main/java/hello/controller/HelloController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;

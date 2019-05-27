f8e69882e4848e327fc5bda6b7052b926c0efe57
Shane Bell <shane.bell@gmail.com>
2019-05-27 10:39:24 +1000

    Disable ErrorMvcAutoConfiguration to trigger GCP Error Reporting

--- src/main/java/hello/servlet/HelloAppEngineApplication.java
+++ src/main/java/hello/servlet/HelloAppEngineApplication.java
@@ -2,9 +2,10 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { ErrorMvcAutoConfiguration.class})
 @ComponentScan("hello")
 public class HelloAppEngineApplication {
 
